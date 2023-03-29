b355ee7d9bcf5a19d118da8535704b5c1b8f334c
Petteri <petu_97@hotmail.com>
2023-03-29 14:38:38 +0300

    Example controller and model removed, added NoteBookController

---
+++ NotebookApp/Controllers/NoteBookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotebookApp.Controllers
+{
+    public class NoteBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
