80150024133f8ed086629b57c7497f7537b92e0c
Petteri <petu_97@hotmail.com>
2023-03-29 15:32:14 +0300

    UnitTest1 file -> NoteBookControllerTests, added few test methods (not implemented), added regions for different function tests

---
+++ TestsProject/NoteBookControllerTests.cs
@@ -0,0 +1,33 @@
+namespace TestsProject
+{
+    public class NoteBookControllerTests
+    {
+        #region GET_GetNoteList
+        [Fact]
+        public void Get_GetNoteList_Returns200OK_ListOfNotes()
+        {
+
+        }
+
+        [Fact]
+        public void Get_GetNoteList_Returns404NotFound_NoItemsFound()
+        {
+
+        }
+
+        #endregion
+
+        #region GET_GetNote
+        #endregion
+
+        #region POST_AddNote
+        #endregion
+
+        #region POST_ModifyNote
+        #endregion
+
+        #region DELETE_RemoveNote
+        #endregion
+
+    }
+}
