b8cf4f768e0ba35accc165274b4c74b3beec0499
Petteri <petu_97@hotmail.com>
2023-03-30 16:26:20 +0300

    Added model for notes (these will be stored to db)

---
+++ BusinessAndDataProject/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BusinessAndDataProject.Models
+{
+    public class Note
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("completed")]
+        public bool Completed;
+
+    }
+}
