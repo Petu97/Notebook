e6e10b566fddfa32ce9ebff664eda0704e2646f6
Petteri <petu_97@hotmail.com>
2023-07-04 14:57:42 +0300

    Database set up (finally). Businessanddataproject: added dbontext,  project depencies and added the db.
    NoteBookApp: added a dbcontext to the database in program.cs file.

--- BusinessAndDataProject/NoteBookDbContext.cs
+++ BusinessAndDataProject/NoteBookDbContext.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessAndDataProject.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace BusinessAndDataProject
 {
     public class NoteBookDbContext : DbContext
     {
+        public DbSet<Note> Notes { get; set; }
+        private const string connectionString = "Data Source=DB\\Database.db"; //hate this hardcoded string, could probably inject iconfiguration from the web project and insert here at some point
 
+        public NoteBookDbContext(DbContextOptions<NoteBookDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString);
+        }
     }
 }
