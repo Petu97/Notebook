778122faf656d753fb08e1afa581dfa51a8fbe59
Petteri <petu_97@hotmail.com>
2023-07-17 16:35:42 +0300

    Controller: updated methods for the updated idatalogic class (WIP) requires implementatiobns. All tests broken.

--- NotebookApp/Controllers/NoteBookController.cs
+++ NotebookApp/Controllers/NoteBookController.cs
@@ -1,23 +1,44 @@
-﻿using BusinessAndDataProject.Models;
+﻿using BusinessAndDataProject.DataLogic;
+using BusinessAndDataProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
 
 namespace NotebookApp.Controllers
 {
     public class NoteBookController : Controller
     {
+        private readonly IDataLogic dataLogic;
 
-        public async Task<IActionResult> GetNoteList(List<Note> ?res) //return all notes
+        public NoteBookController(IDataLogic _dataLogic)
         {
-            var result = res; //add async function for finding notes in database
-
-            if (result is null)
-                return StatusCode(statusCode: 500,"Failed to connect to database");
-
-            else if (!result.Any())
-                return NotFound("You don't have any notes");
-
-            else
-                return Ok(result);   
+            dataLogic = _dataLogic;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNoteList() //return all notes
+        {
+            RequestReturnObject result = await dataLogic.ReturnNotes();
+
+            return StatusCode(statusCode: (int)result.returnState, result.Notes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNote([FromQuery ]int ?id, [FromQuery] string? title)
+        {
+            RequestReturnObject result = await dataLogic.ReturnNotes(); //implement query
+
+            return StatusCode(statusCode: (int)result.returnState, result.Notes);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostNote(JsonObject newItem)
+        {
+            Note newNote = new Note();
+
+            RequestReturnObject result = await dataLogic.ReturnNotes(); //implement query
+
+            return StatusCode(statusCode: (int)result.returnState, result.Notes);
+        }
+
     }
 }
