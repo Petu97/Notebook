854ab3d4e23ac019e58e58b2c39a1c193b2a9174
Petteri <petu_97@hotmail.com>
2023-07-20 16:56:02 +0300

    Created a Datalogic mockup class for unit tests, refactored the initial unit tests to use the new mockup, implementation required

---
+++ TestsProject/TestMockups/Mockup_DataLogic.cs
@@ -0,0 +1,35 @@
+﻿using BusinessAndDataProject.DataLogic;
+using BusinessAndDataProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsProject.TestMockups
+{
+    public class Mockup_DataLogic : IDataLogic
+    {
+        public RequestReturnObject response;
+        public async Task<RequestReturnObject> ReturnNotes()
+        {
+            return response;
+        }
+
+        public async Task<RequestReturnObject> FindNote(int? id, string? title)
+        {
+            return response;
+        }
+
+        public async Task<RequestReturnObject> UpdateNote(Note note)
+        {
+            return response;
+        }
+
+        public async Task<RequestReturnObject> AddNote(Note note)
+        {
+            return response;
+        }
+
+    }
+}
---
+++ TestsProject/UnitTests/NoteBookControllerTests.cs
@@ -0,0 +1,92 @@
+using BusinessAndDataProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using NotebookApp.Controllers;
+using TestsProject.TestMockups;
+
+namespace TestsProject.UnitTests
+{
+    public class NoteBookControllerTests
+    {
+        #region GET_GetNoteList
+
+        [Fact]
+        public async Task GET_Notes_Returns200OK_ListOfNotes()
+        {
+            Mockup_DataLogic mockup = new Mockup_DataLogic();
+            var controller = new NoteBookController(mockup); //arrange
+
+            //List<Note> notes = new List<Note>();
+            //Note note1 = new Note();
+            //Note note2 = new Note();
+            //notes.Add(note1);
+            //notes.Add(note2);
+
+            IActionResult result = await controller.GET_Notes(); //act
+
+            ObjectResult objRes = Assert.IsType<OkObjectResult>(result);  //assert
+            Assert.IsType<List<Note>>(objRes.Value); //object type
+            Assert.Equal(200, objRes.StatusCode); //statuscode
+        }
+
+        [Fact]
+        public async Task GET_Notes_Returns404NotFound_EmptyListOfNotes()
+        {
+            Mockup_DataLogic mockup = new Mockup_DataLogic();
+            var controller = new NoteBookController(mockup); //arrange
+
+            IActionResult result = await controller.GET_Notes();
+
+            ObjectResult objRes = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.IsType<string>(objRes.Value);
+            Assert.Equal(404, objRes.StatusCode);
+        }
+
+        [Fact]
+        public async Task GET_Notes_Returns500InternalServerError_ErrorString()
+        {
+            Mockup_DataLogic mockup = new Mockup_DataLogic();
+            var controller = new NoteBookController(mockup); //arrange
+
+            IActionResult result = await controller.GET_Notes();
+
+            ObjectResult objRes = Assert.IsType<ObjectResult>(result);
+            Assert.IsType<string>(objRes.Value);
+            Assert.Equal(500, objRes.StatusCode);
+        }
+
+        #endregion
+
+        #region GET_GetNote
+
+        //[Fact]
+        //public async Task GET_Note_Return200OK_Note()
+        //{
+        //    var controller = new NoteBookController();
+        //}
+
+        //[Fact]
+        //public async Task GET_Note_Return404NotFound_NoItemsFoundString()
+        //{
+        //    var controller = new NoteBookController();
+
+        //}
+
+        //[Fact]
+        //public async Task GET_Note_Return500InternalServerError_ErrorString()
+        //{
+        //    var controller = new NoteBookController();
+
+        //}
+        #endregion
+
+        #region POST_AddNote
+        #endregion
+
+        #region POST_ModifyNote
+        #endregion
+
+        #region DELETE_RemoveNote
+        #endregion
+
+    }
+}
