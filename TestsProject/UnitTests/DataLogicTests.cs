7d6ab1f66904f805be9b004629e4398620cee59a
Petteri <petu_97@hotmail.com>
2023-08-10 21:00:57 +0300

    Added: Unit test file for datalogic, created test functions for adding notes. Added: integrationtests file for noteworkflow (controller + datalogic + database)

---
+++ TestsProject/UnitTests/DataLogicTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsProject.UnitTests
+{
+    internal class DataLogicTests
+    {
+        #region FindNote
+
+        #endregion
+
+        #region AddNote
+
+        public async Task AddNote_RequestReturnObject_Ok()
+        {
+
+        }
+
+        public async Task AddNote_RequestReturnObject_BadRequest()
+        {
+
+        }
+        public async Task AddNote_RequestReturnObject_InternalError()
+        {
+
+        }
+
+        #endregion
+
+        #region UpdateNote
+
+        #endregion
+
+        #region DeleteNote
+
+        #endregion
+    }
+}
