5ff6c994d8d982141357cc1a78f1f69d762e4308
Petteri <petu_97@hotmail.com>
2023-08-17 14:41:43 +0300

    Program file: added scoped service for idatalogic. Note: i had to change notebookdbcontext connectionstring to hard coded version for now, will change it to relative later on

--- NotebookApp/Program.cs
+++ NotebookApp/Program.cs
@@ -1,4 +1,5 @@
 using BusinessAndDataProject;
+using BusinessAndDataProject.DataLogic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<NoteBookDbContext>();
+builder.Services.AddScoped<IDataLogic, DataLogic>();
 
 
 var app = builder.Build();
